fc67c05c2cd024be6a03432238148278d577748f
gparente3 <giovanni.parente@dxc.com>
2021-12-03 16:14:33 +0100

    added a method that translates a string into a qrcode

--- src/main/java/it/core/facilitator/boot/controller/BootController.java
+++ src/main/java/it/core/facilitator/boot/controller/BootController.java
@@ -1,25 +1,45 @@
 package it.core.facilitator.boot.controller;
 
-import java.io.IOException;
+import javax.validation.constraints.NotBlank;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.log4j.Log4j2;
 
 @RestController
 @Log4j2
-@RequestMapping("/boot")
 public class BootController {
-
-    @GetMapping(value = "/api")
-    public ResponseEntity<Object> checkAssistenza() throws IOException {
-
-      	return new ResponseEntity<Object>("ok", HttpStatus.OK);
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@Value("${api.url}")
+	private String url;
+  	
+	/**
+	 * method that return qrcode png from string
+	 * @param content - text in string format
+	 * @return png resource
+	 */
+  	@GetMapping(value = "/qrcode", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<Resource> qrCodeGenerator(@RequestParam @NotBlank String content) {
+  		log.info("content {}", content);
+        ResponseEntity<byte[]> response = restTemplate.getForEntity(url + content, byte[].class);
+        final ByteArrayResource inputStream = new ByteArrayResource(response.getBody());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentLength(inputStream.contentLength())
+                .body(inputStream);
     }
-
 	
 }
